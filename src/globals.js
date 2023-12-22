c69dd3499103265d6d08bec2ade214cae57b1743
ralvs <renanmalves@gmail.com>
2023-12-22 11:10:34 -0300

    Update globals.js
    To remove the error 
    `ReferenceError: window is not defined`

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,12 @@
-const root = window || this;
-const doc = root.window.document;
+let root, doc;
+
+if (typeof window !== 'undefined') {
+  root = window;
+  doc = root.document;
+} else {
+  // Handle non-browser environment
+  root = globalThis; // Use globalThis for modern environments
+  doc = null; // Set doc to null or handle it differently in non-browser environment
+}
 
 export { root, doc };
